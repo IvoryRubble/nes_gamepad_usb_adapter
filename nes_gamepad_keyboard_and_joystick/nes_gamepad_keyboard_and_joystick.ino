6878e5dcd2b2476a025b92490df18d0a741c49a9
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2025-04-29 23:45:01 +0700

    Added joystick

--- nes_gamepad_keyboard_and_joystick/nes_gamepad_keyboard_and_joystick.ino
+++ nes_gamepad_keyboard_and_joystick/nes_gamepad_keyboard_and_joystick.ino
@@ -1,8 +1,15 @@
 #include <Keyboard.h>
+#include <Joystick.h>
 #include "NesGamepad.h"
 #include "ButtonDebounce.h"
 
-NesGamepad gamepad(A0, A1, A2);
+const int latchPin = A0;
+const int pulsePin = A1;
+const int dataPin = A2;
+const unsigned int delayBeforeReadMicros = 6;
+NesGamepad gamepad(latchPin, pulsePin, dataPin, delayBeforeReadMicros);
+
+Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD, 4, 1, false, false, false, false, false, false, false, false, false, false, false);
 
 unsigned long previousBtnUpdateTime = 0;
 
@@ -28,6 +35,13 @@ const char* btnNames[gamepad.btnsCount] = {
   "Right"
 };
 
+enum ButtonIndex {
+  btnUpIndex = 4,
+  btnDownIndex = 5,
+  btnLeftIndex = 6,
+  btnRightIndex = 7
+};
+
 const uint8_t keysKeyboard[gamepad.btnsCount] = {
   'k',
   'j',
@@ -39,9 +53,22 @@ const uint8_t keysKeyboard[gamepad.btnsCount] = {
   'd'
 };
 
+const uint8_t keysJoystick[gamepad.btnsCount] = {
+  0,
+  1,
+  2,
+  3,
+  0,
+  0,
+  0,
+  0
+};
+
 void setup() {
   Serial.begin(115200);
   gamepad.init();
+  Keyboard.begin();
+  joystick.begin();
   delay(2000);
   Serial.println("Please stand by...");
 }
@@ -57,12 +84,47 @@ void loop() {
   btnDebouces[6].updateState(gamepad.btnLeft);
   btnDebouces[7].updateState(gamepad.btnRight);
 
-  for (int i = 0; i < gamepad.btnsCount; i++) {
+  // for (int i = 0; i < gamepad.btnsCount; i++) {
+  //   if (btnDebouces[i].isBtnPressed) {
+  //     Keyboard.press(keysKeyboard[i]);
+  //   }
+  //   if (btnDebouces[i].isBtnReleased) {
+  //     Keyboard.release(keysKeyboard[i]);
+  //   }
+  // }
+
+  for (int i = 0; i < 4; i++) {
     if (btnDebouces[i].isBtnPressed) {
-      Keyboard.press(keysKeyboard[i]);
+      joystick.pressButton(keysJoystick[i]);
     }
     if (btnDebouces[i].isBtnReleased) {
-      Keyboard.release(keysKeyboard[i]);
+      joystick.releaseButton(keysJoystick[i]);
+    }
+  }
+
+  bool isArrowChanged = false;
+  for (int i = 4; i < gamepad.btnsCount; i++) {
+    isArrowChanged = isArrowChanged || (btnDebouces[i].isBtnPressed || btnDebouces[i].isBtnReleased);
+  }
+  if (isArrowChanged) {
+    if (btnDebouces[btnUpIndex].btnState && btnDebouces[btnRightIndex].btnState) {
+      joystick.setHatSwitch(0, 45);
+    } else if (btnDebouces[btnRightIndex].btnState && btnDebouces[btnDownIndex].btnState) {
+      joystick.setHatSwitch(0, 135);
+    } else if (btnDebouces[btnDownIndex].btnState && btnDebouces[btnLeftIndex].btnState) {
+      joystick.setHatSwitch(0, 225);
+    } else if (btnDebouces[btnLeftIndex].btnState && btnDebouces[btnUpIndex].btnState) {
+      joystick.setHatSwitch(0, 315);
+    } else if (btnDebouces[btnUpIndex].btnState) {
+      joystick.setHatSwitch(0, 0);
+    } else if (btnDebouces[btnRightIndex].btnState) {
+      joystick.setHatSwitch(0, 90);
+    } else if (btnDebouces[btnDownIndex].btnState) {
+      joystick.setHatSwitch(0, 180);
+    } else if (btnDebouces[btnLeftIndex].btnState) {
+      joystick.setHatSwitch(0, 270);
+    } else {
+      joystick.setHatSwitch(0, -1);
     }
   }
 
